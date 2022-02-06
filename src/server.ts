7a78293e67da1a74a5dc5ea7f781bde0df2ffdd7
Antonio Sousa <toninhoplaysson.123@gmail.com>
2022-02-06 10:41:28 -0300

    final project 1.0

--- src/server.ts
+++ src/server.ts
@@ -17,6 +17,6 @@ server.use(express.static(path.join(__dirname, '../public')));
 //rotas
 server.use(mainRoutes);
 server.use((req, res)=>{
-    res.send('Pagina não encontrada');
+    res.render('pages/404');
 });
 server.listen(4000);
